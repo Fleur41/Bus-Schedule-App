e6cc1e13798573a6428249133194a4e71c3182fe
Fleur41 <Muriithikings456@gmail.com>
2024-10-28 13:37:09 +0300

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,11 @@ android {
 }
 
 dependencies {
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
+    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
+    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
 
     implementation(platform("androidx.compose:compose-bom:2023.05.01"))
     implementation("androidx.activity:activity-compose:1.7.2")
